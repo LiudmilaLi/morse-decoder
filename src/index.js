16df41a953b95fd312b468b4d3ff11ff90db6219
LiudmilaLi <LiudmilaLitvin@gmail.com>
2019-10-09 19:38:17 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
 
-module.exports = {
-    decode
-}
+    let letter = {'**********' : ' '}; 
+    let textCode = [];
+    
+                  
+        for (let sign in MORSE_TABLE) {
+            let decodeSymbol = '';
+         for (let i = 0; i < sign.length; i++) {
+                if (sign[i] === '.') {
+                    decodeSymbol += '10';
+                } else decodeSymbol += '11';
+                } 
+                             
+             if (decodeSymbol.length < 10) {
+              letter['0'.repeat(10 - decodeSymbol.length) + decodeSymbol] = MORSE_TABLE[sign];
+            } else {
+              letter[decodeSymbol] = MORSE_TABLE[sign];
+            }
+         }
+          
+          for (let i = 0; i < expr.length; i += 10) {
+               textCode.push(expr.slice(i, i + 10)); 
+               continue;
+              } 
+              
+
+        let exprSym = '';
+        const solution = textCode.reduce((accumulator, dua) => accumulator + letter[dua], exprSym);
+             
+            return solution;             
+         }
+
+        module.exports = {
+            decode
+        }
